c425f81aae0c092b55930876014855d27736412c
Jerrybibo <59254806+Jerrybibo@users.noreply.github.com>
2022-01-17 01:29:30 -0500

    Variable name cleanup, reformatted code

--- convert_motion_to_exp.py
+++ convert_motion_to_exp.py
@@ -1,20 +1,30 @@
-import json
+from json import load, dumps
 
-with open('face_angry_01.motion3.json', 'r') as f:
-    data = json.load(f)
 
-output = {'Type': 'Live2D Expression',
-          'Parameters': []
-          }
-for i in data['Curves']:
-    if i['Target'] == 'Parameter':
-     id = i['Id']
-     value = round(i['Segments'][-1], 3)
-     d = {'Id': id, 'Value': value, 'Blend': 'Add'}
-     output['Parameters'].append(d)
-     print(d)
+def convert_motion_to_exp(motion_file, output_file):
+    output = {'Type': 'Live2D Expression', 'Parameters': []}
+    try:
+        with open(motion_file, 'r') as f:
+            data = load(f)
+    except FileNotFoundError:
+        print(f"File {f} was not found.")
+        exit(-1)
+    for i in data['Curves']:
+        if i['Target'] == 'Parameter':
+            param_id = i['Id']
+            value = round(i['Segments'][-1], 3)
+            d = {'Id': param_id, 'Value': value, 'Blend': 'Add'}
+            output['Parameters'].append(d)
+    output = dumps(output, indent=4)
+    with open(output_file, 'w') as f:
+        f.write(output)
 
-print(output)
-output = json.dumps(output, indent=4)
-with open('test.json', 'w') as f:
-    f.write(output)
+
+def main():
+    motion_file = input("Enter motion file path: ")
+    output_file = motion_file.replace('motion3', 'exp3')
+    convert_motion_to_exp(motion_file, output_file)
+
+
+if __name__ == "__main__":
+    main()
