e7d0a7bc77053fc8ac6994b56f3cec7c37091ee3
Jerrybibo <59254806+Jerrybibo@users.noreply.github.com>
2022-01-17 01:34:35 -0500

    Added docstring to function and error checking

--- convert_motion_to_exp.py
+++ convert_motion_to_exp.py
@@ -2,22 +2,31 @@ from json import load, dumps
 
 
 def convert_motion_to_exp(motion_file, output_file):
+    """
+    Main function to convert file of .motion3.json type to .exp3.json type.
+    :param motion_file: The path to the .motion3.json file
+    :param output_file: The path to the desired output file (must end in .exp3.json)
+    """
+    if not output_file.endswith('.exp3.json'):
+        print(f"The output file path {output_file} is not of the correct type ending.")
+        exit(-1)
     output = {'Type': 'Live2D Expression', 'Parameters': []}
+    # Attempts to open source (motion3) file; exits with exception if not found
     try:
         with open(motion_file, 'r') as f:
             data = load(f)
     except FileNotFoundError:
-        print(f"File {f} was not found.")
+        print(f"File ({f}) was not found.")
         exit(-1)
+    # Grabs relevant data from motion3 file then writes to exp3 file
     for i in data['Curves']:
         if i['Target'] == 'Parameter':
             param_id = i['Id']
             value = round(i['Segments'][-1], 3)
             d = {'Id': param_id, 'Value': value, 'Blend': 'Add'}
             output['Parameters'].append(d)
-    output = dumps(output, indent=4)
     with open(output_file, 'w') as f:
-        f.write(output)
+        f.write(dumps(output, indent=4))
 
 
 def main():
